0372a8488b53a9e59e79c6ebe394a868b7500f81
Radoslav Simeonov <rsimeonovv@gmail.com>
2026-02-01 20:50:10 +0200

    Add Product category and feature repositories

---
+++ ProductCatalog/ProductCatalog.Domain/Catalog/Repositories/IProductCategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using ProductCatalog.Domain.Catalog.Entities;
+
+namespace ProductCatalog.Domain.Catalog.Repositories;
+
+public interface IProductCategoryRepository
+{
+    Task<ProductCategory?> GetByIdAsync(Guid categoryId, CancellationToken cancellationToken = default);
+    Task<ICollection<ProductCategory>> GetAllAsync(CancellationToken cancellationToken = default);
+    void Add(ProductCategory category);
+}
